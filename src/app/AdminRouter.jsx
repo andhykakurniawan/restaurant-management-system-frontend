7a6bd4593f9b6f000b1ef1e150d1b846f523549f
andhykakurniawan <74031708+andhykakurniawan@users.noreply.github.com>
2026-05-07 02:35:09 +0700

    Move all Folder to admin side

--- src/app/AdminRouter.jsx
+++ src/app/AdminRouter.jsx
@@ -4,13 +4,13 @@ import ProtectedRoute from "@/routes/ProtectedRoute";
 import DashboardLayout from "@/components/layout/DashboardLayout";
 import AdminDashboardPage from "@/features/admin/dashboard/pages/AdminDashboardPage";
 import SettingPage from "@/pages/SettingPage";
-import CategoryPage from "@/features/category/CategoryPage";
-import MenuPage from "@/features/menu/MenuPage";
-import IngredientPage from "@/features/ingredient/IngredientPage";
-import MenuBookPage from "@/features/menucategories/MenuBookPage";
-import OrderPage from "@/features/order/OrderPage";
-import PaymentPage from "@/features/payment/PaymentPage";
-import UserPage from "@/features/user/UserPage";
+import CategoryPage from "@/features/admin/category/CategoryPage";
+import MenuPage from "@/features/admin/menu/MenuPage";
+import IngredientPage from "@/features/admin/ingredient/IngredientPage";
+import MenuBookPage from "@/features/admin/menucategories/MenuBookPage";
+import OrderPage from "@/features/admin/order/OrderPage";
+import PaymentPage from "@/features/admin/payment/PaymentPage";
+import UserPage from "@/features/admin/user/UserPage";
 
 export default function AdminRouter() {
     return (
